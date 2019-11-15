a2b96cbfc9e66b85875296ede8c240cab8ba0dfd
rokoga <rokoga@gmail.com>
2019-11-15 14:27:28 -0300

    Initial commit

---
+++ frame/frame.go
@@ -0,0 +1,461 @@
+package frame
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+/*
+ * Um DataFrame representa uma tabela em que as colunas podem ser
+ * agrupadas para gerar aggregações.
+ * As colunas são separadas em colunas pivotáveis e agregáveis.
+ * Por simplificação, todas as colunas pivotáveis será `string`
+ * e todas as colunas agregáveis seráo inteiras.
+ *
+ * As agregações
+ *
+ *	DataFrame{["A", "B", "C"], ["V1", "V2"]}
+ * 	A	B	C	V1	V2
+ * --------------------
+ *	a	b	c	10	100
+ *	a	d	e	20	200
+ *	x	y	z	30	400
+ *
+ * Agg{[A,B]}.Sum(0)	Agg[A].Sum(1)
+ * ===============		=============
+ * 	A	B	SUM			A	SUM
+ *	------------		--------
+ *	a	b	10			a	300
+ * 	a	d	20			x	400
+ *	x	y	30
+ *
+ * pivots := Agg[A].Sum(0)
+ * =======================
+ * Agg[A].Dim() == 1, Agg[A].Pivots() == ["A"]
+ * Agg[A].Distinct() == 2
+ *
+ * 	A	SUM					Agg[A].Count
+ *	--------				------------
+ *	a	30		pivots[0]		2
+ *	x	30		pivots[1]		1
+ *
+ * GetPivotOrNil(pivots, "a") == Pivot{dim: 1, typo: "SUM", pivots: ["a"], value: 30}
+ * GetPivotOrNil(pivots, "x") == Pivot{dim: 1, typo: "SUM", pivots: ["x"], value: 30}
+ */
+
+type DataFrame interface {
+	// As colunas pivotáveis
+	PivotableColumns() []string
+	// As colunas aqregáveis
+	ValuableColumns() []string
+
+	// Adiciona uma linha no DataFrame
+	// Retorna error se não for do shape correto
+	AddRecord(pivots []string, vals []int) error
+	// Adiciona vários records ao DataFrame
+	AddRecords(recs []interface{}) error
+
+	// Agregador das colunas pivots passadas.
+	// pivots: nome das colunas agregadas
+	Agg(pivots ...string) (Agg, error)
+
+	/*
+	 * Métodos funcionais.
+	 */
+
+	// Executa a função para cada linha do dataframe
+	Foreach(func(pivots []string, vals []int))
+	// Mapeia cada linha do dataframe em um valor através da função de mapeamento.
+	Map(func(pivots []string, vals []int) ([]int, error)) DataFrame
+	// Retorna um novo DataFrame sem as linhas sinalizadas pela função filtro.
+	Filter(func(pivots []string, vals []int) (bool, error)) DataFrame
+	// Reduz todo o dataframe a um valor através da função de redução.
+	// A função de redução recebe a redução das linhas anteriores e gera a próxima redução.
+	// A redução retornada será a da última linha.
+	// Ex. de redução: Somar todos os valores das colunas agregáveis.
+	Reduce(func(acum int, pivots []string, vals []int) (int, error)) int
+}
+
+// Agg retorna um subespaço do DataFrame, onde podem ser executadas
+// operações sobre as colunas numéricas.
+type Agg interface {
+	// Dim retorna a quantidade de colunas sumarizáveis.
+	// As colunas sumarizáveis não contam as colunas numéricas.
+	Dim() int
+	// Pivots retorna as colunas sumarizáveis.
+	Pivots() []string
+	Frame() DataFrame
+
+	// Distinct conta a quantidade de pivots diferentes.
+	Distinct() int
+	// Count retorna a quantidade de repetições de cada pivoteamento.
+	// A ordem do array é a mesma dos pivoteamentos gerados por qualquer função de agregação.
+	Count() []int
+	// Sum é uma função de agregação que retorna os pivoteamentos das colunas da agregação.
+	Sum(idx int) ([]*Pivot, error)
+	// No caso de agregação da média deve ser
+	// arredondado o valor para um inteiro.
+	Avg(idx int) ([]*Pivot, error)
+	// Max é uma função de agregação que retorna os pivoteamentos das colunas da agregação.
+	Max(idx int) ([]*Pivot, error)
+	// Min é uma função de agregação que retorna os pivoteamentos das colunas da agregação.
+	Min(idx int) ([]*Pivot, error)
+}
+
+// Pivot é a estrutura que retorna uma instância do pivoteamento
+// (valores das colunas pivot e o valor de uma função de agregação).
+type Pivot struct {
+	dim    int
+	typo   string   // O tipo: SUM | AVG | ...
+	pivots []string // O valor das colunas pivotadas
+	value  int      // O valor da operação de pivotamente sobre as colunas
+}
+
+// Estrutura de cada linha, contendo colunas pivotáveis e colunas agregáveis
+type Row struct {
+	pivots []string
+	vals   []int
+}
+
+// Estrutura de implementação da interface Dataframe
+type dataFrame struct {
+	// nome das colunas pivotáveis
+	namePivots []string
+	// nome das colunas agregáveis
+	nameVals []string
+	rows     []Row
+}
+
+// Estrutura de implementação da interface Agg
+type Aggregation struct {
+	frame dataFrame
+	dim   int
+}
+
+func (agg *Aggregation) Dim() int {
+	return agg.dim
+}
+
+func (agg *Aggregation) Pivots() []string {
+	return agg.frame.namePivots
+}
+
+func (agg *Aggregation) Frame() DataFrame {
+	return &agg.frame
+}
+
+func (agg *Aggregation) Distinct() int {
+	m := make(map[string]int)
+
+	for _, row := range agg.frame.rows {
+		m[strings.Join(row.pivots[:], ",")] += 1
+	}
+
+	return len(m)
+}
+
+// Estrutura auxiliar necessário para manter a ordem/index nos maps
+type NavigationMap struct {
+	m    map[string]int
+	keys []string
+}
+
+// Adiciona uma chave valor
+func (n *NavigationMap) Set(k string, v int) {
+	_, present := n.m[k]
+	n.m[k] = v
+	// Caso a chave já está presente não é adicionado como novo elemento
+	if !present {
+		n.keys = append(n.keys, k)
+	}
+}
+
+// cria/inicializa um novo NavigationMap
+func NewNavigationMap() *NavigationMap {
+	var navMap NavigationMap
+	navMap.m = make(map[string]int)
+	navMap.keys = make([]string, 0)
+	return &navMap
+}
+
+func (agg *Aggregation) Count() []int {
+	navMap := NewNavigationMap()
+
+	for _, row := range agg.frame.rows {
+		key := strings.Join(row.pivots[:], ",")
+		navMap.Set(key, navMap.m[key]+1)
+	}
+
+	keys := []int{}
+	for _, value := range navMap.keys {
+		keys = append(keys, navMap.m[value])
+	}
+
+	return keys
+}
+
+func checkOutOfBounds(idx int, max int) error {
+	if idx < 0 || idx > max {
+		return fmt.Errorf("index %d out of bounds [0:%d]", idx, max)
+	}
+	return nil
+}
+
+func (agg *Aggregation) Sum(idx int) ([]*Pivot, error) {
+
+	if err := checkOutOfBounds(idx, len(agg.frame.nameVals)); err != nil {
+		return nil, err
+	}
+
+	pivots := []*Pivot{}
+
+	navMap := NewNavigationMap()
+
+	for _, row := range agg.frame.rows {
+		key := strings.Join(row.pivots[:], ",")
+		navMap.Set(key, navMap.m[key]+row.vals[idx])
+	}
+
+	for _, value := range navMap.keys {
+		p := strings.Split(value, ",")
+		pivots = append(pivots, &Pivot{dim: len(p), typo: "SUM", pivots: p, value: navMap.m[value]})
+	}
+
+	return pivots, nil
+}
+
+func (agg *Aggregation) Avg(idx int) ([]*Pivot, error) {
+
+	if err := checkOutOfBounds(idx, len(agg.frame.nameVals)); err != nil {
+		return nil, err
+	}
+
+	pivots := []*Pivot{}
+
+	navMap := NewNavigationMap()
+	navMapCount := NewNavigationMap()
+
+	for _, row := range agg.frame.rows {
+		key := strings.Join(row.pivots[:], ",")
+		navMap.Set(key, navMap.m[key]+row.vals[idx])
+		navMapCount.Set(key, navMapCount.m[key]+1)
+	}
+
+	for _, value := range navMap.keys {
+		p := strings.Split(value, ",")
+		pivots = append(pivots, &Pivot{dim: len(p), typo: "AVG", pivots: p, value: navMap.m[value] / navMapCount.m[value]})
+	}
+
+	return pivots, nil
+}
+
+func (agg *Aggregation) Max(idx int) ([]*Pivot, error) {
+
+	if err := checkOutOfBounds(idx, len(agg.frame.nameVals)); err != nil {
+		return nil, err
+	}
+
+	var maxPivot []*Pivot
+
+	max := agg.frame.rows[0].vals[idx]
+
+	for _, row := range agg.frame.rows {
+		if max <= row.vals[idx] {
+			if max != row.vals[idx] {
+				maxPivot = nil
+			}
+			max = row.vals[idx]
+			maxPivot = append(maxPivot, &Pivot{dim: 1, typo: "MAX", pivots: row.pivots, value: max})
+		}
+	}
+
+	return maxPivot, nil
+}
+
+func (agg *Aggregation) Min(idx int) ([]*Pivot, error) {
+
+	if err := checkOutOfBounds(idx, len(agg.frame.nameVals)); err != nil {
+		return nil, err
+	}
+
+	var minPivot []*Pivot
+
+	min := agg.frame.rows[0].vals[idx]
+
+	for _, row := range agg.frame.rows {
+		if min >= row.vals[idx] {
+			if min != row.vals[idx] {
+				minPivot = nil
+			}
+			min = row.vals[idx]
+			minPivot = append(minPivot, &Pivot{dim: 1, typo: "MIN", pivots: row.pivots, value: min})
+		}
+	}
+
+	return minPivot, nil
+}
+
+func (df *dataFrame) AddRecord(pivots []string, vals []int) error {
+	if len(pivots) > len(df.namePivots) {
+		return fmt.Errorf("pivot length %d exceed the Dataframe pivot current size %d", len(pivots), len(df.namePivots))
+	} else if len(vals) > len(df.nameVals) {
+		return fmt.Errorf("vals length %d exceed the Datafram val current size %d", len(vals), len(df.nameVals))
+	}
+
+	df.rows = append(df.rows, Row{pivots, vals})
+	return nil
+
+}
+
+func (df *dataFrame) AddRecords(recs []interface{}) error {
+	var err error
+
+	newDf := df
+	for _, record := range recs {
+		row, ok := record.(Row)
+		if !ok {
+			err = fmt.Errorf("Record %v doesn't have the Row struct: {pivots []string vals int[]}", row)
+			break
+		}
+
+		if addRecordErr := newDf.AddRecord(row.pivots, row.vals); addRecordErr != nil {
+			err = addRecordErr
+			break
+		}
+	}
+	// If there's an error on adding records, return the error
+	if err != nil {
+		return err
+	}
+
+	df = newDf
+
+	return nil
+}
+
+// Obtém o index dos pivots de acordo com as strings passadas de argumento
+func (df *dataFrame) GetPivotsIdx(pivots ...string) ([]int, error) {
+	idx := []int{}
+	found := false
+
+	for _, pivotName := range pivots {
+		found = false
+		for index, dfPivotName := range df.namePivots {
+			if pivotName == dfPivotName {
+				idx = append(idx, index)
+				found = true
+				break
+			}
+		}
+		if !found {
+			return nil, fmt.Errorf("Not found pivot %s", pivotName)
+		}
+	}
+
+	return idx, nil
+}
+
+func NewAgg(df *dataFrame, pivot []string, vals []string) (Agg, error) {
+
+	newDf, ok := NewDataFrame(pivot, vals).(*dataFrame)
+
+	if !ok {
+		return nil, errors.New("Error converting Type dataFrame")
+	}
+
+	pivotIdx, err := df.GetPivotsIdx(pivot...)
+	if err != nil {
+		return nil, err
+	}
+
+	record := []string{}
+	for _, row := range df.rows {
+		for _, idx := range pivotIdx {
+			record = append(record, row.pivots[idx])
+		}
+		newDf.AddRecord(record, row.vals)
+		record = nil
+	}
+
+	return &Aggregation{*newDf, len(pivot)}, nil
+}
+
+func (df *dataFrame) Agg(pivots ...string) (Agg, error) {
+	newAgg, err := NewAgg(df, pivots, df.nameVals)
+	if err != nil {
+		return nil, err
+	}
+	return newAgg, nil
+}
+
+func (df *dataFrame) Foreach(cb func(pivots []string, vals []int)) {
+	for i, _ := range df.rows {
+		cb(df.rows[i].pivots, df.rows[i].vals)
+	}
+}
+
+func (df *dataFrame) Map(cb func(pivots []string, vals []int) ([]int, error)) DataFrame {
+	newDf := NewDataFrame(df.namePivots, df.nameVals)
+	df.Foreach(func(pivots []string, vals []int) {
+		if newVal, err := cb(pivots, vals); err == nil {
+			newDf.AddRecord(pivots, newVal)
+		}
+	})
+	return newDf
+}
+
+func (df *dataFrame) Filter(cb func(pivots []string, vals []int) (bool, error)) DataFrame {
+	newDf := NewDataFrame(df.namePivots, df.nameVals)
+	df.Foreach(func(pivots []string, vals []int) {
+		if check, err := cb(pivots, vals); err == nil && check == true {
+			newDf.AddRecord(pivots, vals)
+		}
+	})
+	return newDf
+}
+
+func (df *dataFrame) Reduce(cb func(acum int, pivots []string, vals []int) (int, error)) int {
+	// harcoded intialize in 0, should have an option to initialize accumulator by arguments
+	accumulator := 0
+	df.Foreach(func(pivots []string, vals []int) {
+		if value, err := cb(accumulator, pivots, vals); err == nil {
+			accumulator += value
+		}
+	})
+	return accumulator
+}
+
+func (df *dataFrame) PivotableColumns() []string {
+	return df.namePivots
+}
+
+func (df *dataFrame) ValuableColumns() []string {
+	return df.nameVals
+}
+
+func NewDataFrame(pivots []string, vals []string) DataFrame {
+	return &dataFrame{pivots, vals, nil}
+}
+
+// GetPivotOrNil é uma Função `utilitária` para retornar determinado Pivot, recebendo o array
+// de pivots gerado pelo Agg.Sum, por ex.
+// Params:
+// 		pivots: array de Pivot gerado por uma função de agregação.
+// 		pivotation, sequência de valores que identificam uma instância do pivoteamente.
+func GetPivotOrNil(pivots []*Pivot, pivotation ...string) *Pivot {
+	var p *Pivot
+
+	found := false
+	for _, pivot := range pivots {
+		if strings.Join(pivot.pivots, ",") == strings.Join(pivotation, ",") {
+			p = pivot
+			found = true
+		}
+	}
+
+	if !found {
+		return nil
+	}
+	return p
+}
---
+++ frame/frame_test.go
@@ -0,0 +1,210 @@
+package frame
+
+/*
+ * Os testes unitários devem cobrir os principais métodos do package,
+ * de forma a ter uma cobertura de, no mínimo, 60%, alferidos com:
+ *      `go test -cover`.
+ *
+ * Os testes unitários pode ser estruturados como melhor convier ao
+ * desenvolvedor, podendo ser implementado mais de um método de testes,
+ * utilizados métodos e estruturas de suporte.
+ *
+ * Exige-se o uso do pacote: `github.com/stretchr/testify/assert`, para
+ * testar valores.
+ */
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Testar um ou mais métodos do DataFrame.
+func TestAggDataFrame(t *testing.T) {
+	assert := assert.New(t)
+
+	// Cria um dataframe com duas colunas pivotáveis, e duas colunas de valores
+	df := NewDataFrame([]string{"A", "B", "C"}, []string{"V1", "V2"})
+	// Verifica adição de novo record com Shape correto
+	err := df.AddRecord([]string{"a", "b", "c"}, []int{10, 100})
+	assert.Nil(err)
+
+	//  Verifica adição de novo record com Shape incorreto
+	err = df.AddRecord([]string{"a", "b", "c", "d"}, []int{0, 0})
+	assert.NotNil(err)
+
+	// Adiciona alguns records
+	df.AddRecord([]string{"a", "d", "c"}, []int{10, 100})
+	df.AddRecord([]string{"x", "y", "z"}, []int{20, 200})
+	df.AddRecord([]string{"i", "j", "k"}, []int{40, 400})
+	df.AddRecord([]string{"i", "j", "k"}, []int{30, 400})
+
+	// Realiza agrupamento por colunas "A" e "B"
+	agg, err := df.Agg("A", "B")
+	// Verifica se não ocorre erro na agregação
+	assert.Nil(err)
+
+	agg2, err2 := df.Agg("A", "C")
+	assert.Nil(err2)
+	assert.NotNil(agg2)
+
+	// Verifica erro ao agregar por coluna inválida
+	agg3, err3 := df.Agg("A", "D")
+	assert.NotNil(err3)
+	assert.Nil(agg3)
+
+	// Testa a soma da coluna V3 inexistente
+	pivots, err := agg.Sum(3)
+	assert.NotNil(err)
+
+	// Testa a soma da coluna V1
+	pivots, _ = agg.Sum(0)
+	v1_ab := GetPivotOrNil(pivots, "a", "b")
+	assert.Equal(10, v1_ab.value)
+
+	// Testa a soma da coluna V2
+	pivots, _ = agg.Sum(1)
+	v2_ab := GetPivotOrNil(pivots, "a", "b")
+	assert.Equal(100, v2_ab.value)
+
+	// Testa a média da coluna V1
+	pivots, _ = agg.Avg(0)
+	avg_v1 := GetPivotOrNil(pivots, "i", "j")
+	assert.Equal(35, avg_v1.value)
+
+	// Testa max da coluna v1
+	pivots, _ = agg.Max(0)
+	max_v1 := GetPivotOrNil(pivots, "i", "j")
+	assert.Equal(40, max_v1.value)
+
+	// Testa mínimo da coluna v1
+	pivots, _ = agg.Min(0)
+	expected := [][]string{{"a", "b"}, {"a", "d"}}
+	for i, pivot := range pivots {
+		assert.Equal(expected[i], pivot.pivots)
+	}
+	// Testa pivot não existente
+	min_v1 := GetPivotOrNil(pivots, "i", "j")
+	var p *Pivot
+	assert.Equal(p, min_v1)
+
+}
+
+// Testa os métodos funcionais
+func TestFuncDataFrame(t *testing.T) {
+	assert := assert.New(t)
+
+	// Verifica a criação de df
+	df := NewDataFrame([]string{"A", "B", "C"}, []string{"V1", "V2"})
+
+	// Verifica a adição de records
+	df.AddRecord([]string{"a", "b", "c"}, []int{10, 100})
+	df.AddRecord([]string{"a", "d", "c"}, []int{10, 100})
+	df.AddRecord([]string{"x", "y", "z"}, []int{20, 200})
+	df.AddRecord([]string{"i", "j", "k"}, []int{30, 300})
+	df.AddRecord([]string{"i", "j", "k"}, []int{40, 400})
+
+	pivotableColumns := df.PivotableColumns()
+	// Verifica se o slice de strings está correto
+	expected := []string{"A", "B", "C"}
+	assert.Equal(expected, pivotableColumns)
+
+	valuableColumns := df.ValuableColumns()
+	assert.Equal(2, len(valuableColumns))
+	// Verifica se o slice de strings
+	expected = []string{"V1", "V2"}
+	assert.Equal(expected, valuableColumns)
+
+	// Testa funcionalidade forEach
+	count := 0
+	df.Foreach(func(pivots []string, vals []int) {
+		assert.Equal([][]string{
+			[]string{"a", "b", "c"},
+			[]string{"a", "d", "c"},
+			[]string{"x", "y", "z"},
+			[]string{"i", "j", "k"},
+			[]string{"i", "j", "k"}}[count], pivots)
+
+		assert.Equal([]int{10, 10, 20, 30, 40}[count], vals[0])
+		assert.Equal([]int{100, 100, 200, 300, 400}[count], vals[1])
+		count++
+	})
+
+	// Transforma o df dividindo a segunda coluna por 10
+	// o que resulta em um df com os valores das colunas iguais.
+	df.Map(func(pivots []string, vals []int) ([]int, error) {
+		return []int{vals[0], int(vals[1] / 10)}, nil
+	}).Foreach(func(pivots []string, vals []int) {
+		assert.Equal(vals[0], vals[1])
+	})
+
+	// Transforma o df removendo as linhas que contiverem a primeira coluna
+	// menor que 20 o que resulta de um dataframe com 3 linhas
+	newDf := df.Filter(func(pivots []string, vals []int) (bool, error) {
+		if vals[0] <= 20 {
+			return true, nil
+		}
+		return false, nil
+	})
+
+	// verifica se retornou o um novo DataFrame
+	assert.NotNil(newDf)
+
+	count = 0
+	newDf.Foreach(func(pivots []string, vals []int) {
+		assert.Equal([][]string{
+			[]string{"a", "b", "c"},
+			[]string{"a", "d", "c"},
+			[]string{"x", "y", "z"}}[count], pivots)
+		assert.Equal([]int{10, 10, 20}[count], vals[0])
+		assert.Equal([]int{100, 100, 200}[count], vals[1])
+		count++
+	})
+	assert.Equal(3, count)
+
+	// Retorna a soma da primeira coluna utilizando reduce
+	sumV1 := df.Reduce(func(accum int, pivots []string, vals []int) (int, error) {
+		return vals[0], nil
+	})
+	assert.Equal(110, sumV1)
+
+	// Retorna a soma da segunda coluna utilizando reduce
+	sumV2 := df.Reduce(func(accum int, pivots []string, vals []int) (int, error) {
+		return vals[1], nil
+	})
+	assert.Equal(1100, sumV2)
+
+	// Adiciona dois records ao df
+	var rows []interface{}
+	rows = append(rows, Row{[]string{"a", "b", "c", "d"}, []int{10, 100, 200}})
+	rows = append(rows, Row{[]string{"a", "d", "c", "d"}, []int{10, 100, 200}})
+	df2 := NewDataFrame([]string{"A", "B", "C"}, []string{"V1", "V2"})
+	df2.AddRecords(rows)
+
+	count = 0
+	df2.Foreach(func(pivots []string, vals []int) {
+		assert.Equal([][]string{
+			[]string{"a", "b", "c", "d"},
+			[]string{"a", "d", "c", "d"}}[count], pivots)
+		assert.Equal([]int{10, 100, 200}[count], vals[0])
+		assert.Equal([]int{10, 100, 200}[count], vals[1])
+		count++
+	})
+
+	// Caso ocorra algum erro na inserção dos records, nenhum record deverá ser inserido
+	rows = nil
+	rows = append(rows, Row{[]string{"a", "b", "c", "d"}, []int{10, 100, 200}})
+	rows = append(rows, Row{[]string{"a", "d", "c", "d", "e"}, []int{10, 100, 200}})
+	df3 := NewDataFrame([]string{"A", "B", "C"}, []string{"V1", "V2"})
+	err := df3.AddRecords(rows)
+
+	// Verifica que ocorre erro e nenhum record deverá ser inserido no df
+	if assert.NotNil(err) {
+		count = 0
+		df3.Foreach(func(pivots []string, vals []int) {
+			count++
+		})
+		assert.Equal(count, 0)
+	}
+
+}
